aba341bacd9c7b9e01cd827a66741a3b5415d8df
pecuenban <pedrocuenca95@yahoo.es>
2020-06-05 21:58:05 +0200

    Ver espacio

--- src/app/espacio/espacio.component.ts
+++ src/app/espacio/espacio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConexionesService } from '../conexiones.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-espacio',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./espacio.component.css']
 })
 export class EspacioComponent implements OnInit {
-
-  constructor() { }
+sala;
+  constructor(protected conexionesService: ConexionesService,
+    private router: Router,
+    private route: ActivatedRoute,) { }
 
   ngOnInit() {
+      this.route.paramMap.subscribe(params => {
+
+
+ this.conexionesService.getFinca('RYz6AHItbzMHLLJA3id7')
+    .subscribe(
+      (data) => { // Success
+        this.sala = data
+        this.sala = this.sala.Salas[params.get("id")];
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+        
+
+        
+    });
+    
+
   }
 
 }
